cbbfd681e6d8d4882595bab584d7deb3f2d742f9
Steingrim Dovland <steingrim@fiken.no>
2019-07-18 21:08:10 +0200

    La på index på spor og rad i utskriftene

--- src/main/kotlin/com/github/steingrd/kabal/print.kt
+++ src/main/kotlin/com/github/steingrd/kabal/print.kt
@@ -27,19 +27,39 @@ fun skrivUt(kabal: Kabal) {
     println("Bunke: ${kabal.bunke}")
     println()
 
+    print("    ")
     kabal.mål.målSpor.values.map { l -> if (l.kort.isEmpty()) "------- " else l.kort.last().toString() + " " }.forEach(::print)
     println()
     println()
 
     val maksLinjer = kabal.spor.liste.map { e ->  e.value.topp.size + e.value.bunn.size}.max()
-    val linjer = (0 until maksLinjer!!).map { StringBuilder() }
-    kabal.spor.liste.forEach { (_, spor) -> skrivUtSpor(spor, linjer) }
+    val linjer = (0 until maksLinjer!!).map { StringBuilder().append("$it".padStart(2, ' ')).append(": ") }
+    (0 until 7).forEach { skrivUtSpor(it, kabal.spor.liste[it] ?: error("Ugyldig index"), linjer) }
+    println("    0       1       2       3       4       5       6")
     linjer.forEach { println(it) }
 
     println()
 }
 
-fun skrivUtSpor(spor: Spor, linjer: List<StringBuilder>) {
-    spor.bunn.forEachIndexed { index, _ -> linjer[index].append("XXXXXXX ") }
-    spor.topp.forEachIndexed { index, kort -> linjer[index + spor.bunn.size].append(kort).append(" ") }
+fun skrivUtSpor(sporIndex: Int, spor: Spor, linjer: List<StringBuilder>) {
+    // fyll evt på med blanke på alle linjer først
+    linjer.forEach {
+        while (sporIndex * 8 > fjernKontroltegn(it).length) {
+            it.append("        ")
+        }
+    }
+    spor.bunn.forEachIndexed { index, _ ->
+        linjer[index].append("XXXXXXX ")
+    }
+    spor.topp.forEachIndexed { index, kort ->
+        linjer[index + spor.bunn.size].append(kort).append(" ")
+    }
+}
+
+fun fjernKontroltegn(stringBuilder: StringBuilder): String {
+    return stringBuilder.toString()
+            .replace(Regex("\\P{Print}"), "")
+            .replace("[31m", "")
+            .replace("[30m", "")
+            .replace("[0m", "")
 }
